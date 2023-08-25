54e87009cc862423c153d8c72013a10632d2ae65
Jacob Medina <jacobmedina1618@gmail.com>
2023-08-25 06:17:28 -0400

    home page touch up and dashboard no posts message

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,6 @@
 function init() {
+    checkAmountPosts();
+    
     $('.edit-post-btn').on('click', handleEditPost);
     $('.delete-post-btn').on('click', handleDeletePost);
 }
@@ -21,6 +23,18 @@ async function handleDeletePost() {
     }
 
     $(`.blog-card-container[data-post-id='${postId}']`).remove();
+
+    checkAmountPosts();
+}
+
+function checkAmountPosts() {
+    if ($('.dashboard-grid-container').children().length <= 1) {
+        $('.no-posts').removeClass('d-none');
+    }
+
+    else {
+        $('.no-posts').addClass('d-none');
+    }
 }
 
 $(init());
